c5ed1ea2a18ba5fe38cccf320ccf07a9751d50b9
AlanKeeps <alan.keeps.walking@gmail.com>
2022-07-17 03:24:05 +0300

    viewModel class added

---
+++ app/src/main/java/com/example/shoppinglist/fragments/NoteFragment.kt
@@ -0,0 +1,43 @@
+package com.example.shoppinglist.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.shoppinglist.R
+import com.example.shoppinglist.activities.MainApp
+import com.example.shoppinglist.databinding.FragmentNoteBinding
+import com.example.shoppinglist.databinding.NoteListItemBinding
+import com.example.shoppinglist.db.MainViewModel
+
+class NoteFragment : BaseFragment() {
+    private lateinit var binding: FragmentNoteBinding
+    private val mainViewModel: MainViewModel by activityViewModels {
+        MainViewModel.MainViewModelFactory((context?.applicationContext as MainApp).database)
+    }
+
+    override fun onClickNew() {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentNoteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = NoteFragment()
+    }
+}
