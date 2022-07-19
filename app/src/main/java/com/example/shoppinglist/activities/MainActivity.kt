57aaa0ba7cb6210541d2ec986eb769add6ae8c05
AlanKeeps <alan.keeps.walking@gmail.com>
2022-07-20 00:12:03 +0300

    add NewListDialog for adding new list

--- app/src/main/java/com/example/shoppinglist/activities/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/activities/MainActivity.kt
@@ -5,10 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import com.example.shoppinglist.R
 import com.example.shoppinglist.databinding.ActivityMainBinding
+import com.example.shoppinglist.dialogs.NewListDialog
 import com.example.shoppinglist.fragments.FragmentManager
 import com.example.shoppinglist.fragments.NoteFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NewListDialog.Listener {
 
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,10 +32,15 @@ class MainActivity : AppCompatActivity() {
                     Log.d("MyLog", "list")
                 }
                 R.id.new_item ->{
-                    FragmentManager.currentFrag?.onClickNew()
+                    //FragmentManager.currentFrag?.onClickNew()
+                    NewListDialog.showDialog(this, this)
                 }
             }
             true
         }
+    }
+
+    override fun onClick(name: String) {
+        Log.d("MyLog", "Name : $name")
     }
 }
